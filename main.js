932b631940d01d3cbf58895a5e33129ae9d8101d
Will Avudim <will.avudim@gmail.com>
2016-11-27 16:31:22 -0800

    Developified.

--- main.js
+++ main.js
@@ -5,36 +5,58 @@ require, __dirname
 */
 
 const bluebird = require('bluebird');
-const fs = bluebird.promisifyAll(require('fs'));
+const fs = bluebird.promisifyAll(require('fs'));  // https://nodejs.org/api/fs.html
+const fse = bluebird.promisifyAll(require('fs-extra'));  // https://www.npmjs.com/package/fs-extra
 const path = require('path');
-const timers = bluebird.promisifyAll(require('timers'));
 const pug = require('pug');
 
 // ------------------------------------------------------------
-// Copy browser dependencies.
-const fse = require('fs-extra');
-const fseasync = bluebird.promisifyAll(fse);
-
-fseasync.ensureDirAsync('libs').then(() => {
-  return Promise.all([
-    CopyFile('node_modules/vue/dist/vue.js', 'libs/vue.js'),
-    CopyFile('node_modules/jquery/dist/jquery.js', 'libs/jquery.js')
-    // CopyFile('node_modules/dropbox/dist/dropbox-sdk.js', 'libs/dropbox-sdk.js')
-  ]);
-}).then(() => { 
-  console.log('Static javascript dependencies copied.');
+// Prepare everything for mocks.
+fse.ensureDirAsync('libs').then(() => Promise.all([
+  fse.copyAsync('node_modules/vue/dist/vue.js', 'libs/vue.js'),
+  fse.copyAsync('node_modules/jquery/dist/jquery.js', 'libs/jquery.js')
+  // fse.copyAsync('node_modules/dropbox/dist/dropbox-sdk.js', 'libs/dropbox-sdk.js')
+])).then(() => { 
+  console.log('Static javascript dependencies copied for mocks.');
 }).catch(err => {  // eslint-disable-line 
-  debugger; 
+  debugger;
 });
 
-function CopyFile(from, to) {
-  return new Promise((resolve, reject) => {
-    fse.copy(from, to, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
+// ------------------------------------------------------------
+// Static compilation of .vue components.
+const static_vue_compiler = require('./modules/static-vue-compiler.js');
+static_vue_compiler('mock/components', 'public/gens');
+
+// ------------------------------------------------------------
+// Prepare all dependencies for development.
+fse.ensureDirAsync('public/libs').then(() => Promise.all([
+  fse.copyAsync('node_modules/vue/dist/vue.js', 'public/libs/vue.js'),
+  fse.copyAsync('node_modules/jquery/dist/jquery.js', 'public/libs/jquery.js'),
+  fse.copyAsync('node_modules/dropbox/dist/dropbox-sdk.js', 'public/libs/dropbox-sdk.js')
+])).then(() => { 
+  console.log('Static javascript dependencies copied for development.');
+}).catch(err => {  // eslint-disable-line 
+  debugger;
+});
+
+// ------------------------------------------------------------
+// Serving. 
+const express = require('express');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Views are pug templates: resnpose.render(...).
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function (request, response) {
+  // Render with a parameter: response.render('index', { title: 'First App!' });
+  response.render('index', {});
+});
+
+app.listen(3000);
+
+// ------------------------------------------------------------
+// Open the browser for development.
+const opn = require('opn');
+opn('http://127.0.0.1:3000/');
---
+++ modules/static-vue-compiler.js
@@ -0,0 +1,78 @@
+'use strict';
+/* eslint no-console:0, quotes:0, no-debugger:0 */
+/* global
+require, module
+*/
+
+const bluebird = require('bluebird');
+const fs = bluebird.promisifyAll(require('fs'));  // https://nodejs.org/api/fs.html
+const fse = bluebird.promisifyAll(require('fs-extra'));  // https://www.npmjs.com/package/fs-extra
+const timers = bluebird.promisifyAll(require('timers'));
+const pug = require('pug');
+const path = require('path');
+const template_compiler = require('vue-template-compiler');
+
+// Watches the specified dir. 
+// Calls OnChange(full_file_path) when that file is first discovered or changed.
+// No action on file removals, unless they are re-introduced back again.
+function WatchDir(dir, OnChange) {
+  const file_list = {};
+  function ScanDir() {
+    fs.readdirAsync(dir).then(files => {
+      for (const file of files) {
+        if (!file.endsWith('.vue')) {
+          continue;
+        }
+
+        // Check the last modification (m)time.
+        const full_path = dir + '/' + file;
+        fs.statAsync(full_path).then(function (stats) {
+          if (file_list[full_path] !== stats.mtime.toJSON()) {
+            file_list[full_path] = stats.mtime.toJSON();
+            OnChange(full_path);
+          }
+        }).catch(e => { console.error('Unable to stat dir', dir, e); });
+      }
+    }).catch(e => { console.error('Unable to scan dir', dir, e); });
+  }
+
+  ScanDir();
+  timers.setInterval(ScanDir, 3000);
+}
+
+// Compiles all updated .vue components into static renderers.
+function CompileVueComponents(path_to_vue_components, path_to_output_modules) {
+  fse.ensureDirAsync(path_to_output_modules).then(() => {
+    WatchDir(path_to_vue_components, filename => {
+      fs.readFileAsync(filename, 'utf8').then(file_content => {
+        const pug_content = template_compiler.parseComponent(file_content).template.content;
+        const html = pug.render(pug_content);
+        const renderers = template_compiler.compile(html);
+
+        // Put into different files as
+        // RegisterMe(component-name, render, staticRenderFns);
+        //+ scan for changes off timer & update; full scan in the beginning
+        const component_name = path.basename(filename, '.vue');
+        let static_fns = '';
+        for (const static_fn of renderers.staticRenderFns) {
+          if (static_fns.length > 0) {
+            static_fns += ',';
+          }
+          static_fns += `function() { ${static_fn} }`;
+        }
+
+        const register_render_code = `
+          RegisterRender(
+            "${component_name}",
+            function() { ${renderers.render} }, 
+            [ ${static_fns} ]
+          );`;
+        const target_file = path_to_output_modules + '/' + component_name + '.js';
+        return fs.writeFileAsync(target_file, register_render_code)
+          .then(() => { console.info("Successfully compiled:", filename); });
+      }).catch(e => { console.error('Unable to read file', filename, e); });
+    });
+  }).catch(e => { console.error('fse.ensureDir failed', path_to_output_modules, e); });
+}
+
+module.exports = CompileVueComponents;
